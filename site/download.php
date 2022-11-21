55ad8481c82b7b546ea50ecf6301000ddb4eb43f
Lukas Dziadkowiec <ldziadkowiec@vendavo.com>
2022-11-21 22:39:34 +0100

    refactor

--- site/download.php
+++ site/download.php
@@ -12,32 +12,31 @@
  * @link     https://www.ovjih.net
  */
 
-require_once(dirname(__FILE__) . "/../includes/Core.php");
+require_once __DIR__ . "/../includes/Core.php";
 $core = new Core();
-require_once($core->getAppRoot() . "includes/Constants.php");
-require_once($core->getAppRoot() . "includes/AppContext.php");
-require_once($core->getAppRoot() . "includes/Database.php");
-require_once($core->getAppRoot() . "includes/utils/Utils.php");
-require_once($core->getAppRoot() . "includes/Mainframe.php");
-require_once($core->getAppRoot() . "includes/utils/DateUtil.php");
-require_once($core->getAppRoot() . "includes/dao/EmailListDAO.php");
-require_once($core->getAppRoot() . "includes/dao/SessionDAO.php");
-require_once($core->getAppRoot() . "includes/tcpdf/tcpdf.php");
+require_once $core->getAppRoot() . "includes/Constants.php";
+require_once $core->getAppRoot() . "includes/AppContext.php";
+require_once $core->getAppRoot() . "includes/Database.php";
+require_once $core->getAppRoot() . "includes/utils/Utils.php";
+require_once $core->getAppRoot() . "includes/Mainframe.php";
+require_once $core->getAppRoot() . "includes/utils/DateUtil.php";
+require_once $core->getAppRoot() . "includes/dao/EmailListDAO.php";
+require_once $core->getAppRoot() . "includes/dao/SessionDAO.php";
 
 try {
-	$database = new Database(
-		$core->getProperty(Core::DATABASE_HOST),
-		$core->getProperty(Core::DATABASE_USERNAME),
-		$core->getProperty(Core::DATABASE_PASSWORD),
-		$core->getProperty(Core::DATABASE_NAME)
-	);
+    $database = new Database(
+        $core->getProperty(Core::DATABASE_HOST),
+        $core->getProperty(Core::DATABASE_USERNAME),
+        $core->getProperty(Core::DATABASE_PASSWORD),
+        $core->getProperty(Core::DATABASE_NAME)
+    );
 } catch (Exception $e) {
-	$core->alert('_("Cannot connect to database")');
-	exit();
+    $core::alert('_("Cannot connect to database")');
+    exit();
 }
 
-$option = strtolower(Utils::getParam($_REQUEST, 'option', 'com_admin'));
-$task = Utils::getParam($_REQUEST, 'task', null);
+$option = $_REQUEST['option'] ??= 'com_admin';
+$task = $_REQUEST['task'];
 
 // must start the session before we create the mainframe object
 session_name("NETPROVIDER");
@@ -47,39 +46,44 @@ session_start();
 $mainframe = new MainFrame($database, $option, '..', null);
 
 $session = new Session();
-$session->SE_sessionid = Utils::getParam($_SESSION, 'SE_sessionid', '');
-$session->SE_personid = Utils::getParam($_SESSION, 'SE_personid', '');
-$session->SE_username = Utils::getParam($_SESSION, 'SE_username', '');
-$session->SE_acl = Utils::getParam($_SESSION, 'SE_acl', '');
-$session->SE_time = Utils::getParam($_SESSION, 'SE_time', '');
+$session->SE_sessionid = $_SESSION['SE_sessionid'] ?? '';
+$session->SE_personid = $_SESSION['SE_personid'] ?? '';
+$session->SE_username = $_SESSION['SE_username'] ?? '';
+$session->SE_acl = $_SESSION['SE_acl'] ?? '';
+$session->SE_time = $_SESSION['SE_time'] ?? '';
 
 // timeout old sessions
-//
 $sessions = SessionDAO::removeTimeoutedSession(1800);
 
 // check against db record of session
-if ($session->SE_sessionid != md5("$session->SE_username$session->SE_acl$session->SE_time") || !SessionDAO::checkSession($session)) {
-	Core::redirect("index.php");
+try {
+    if ($session->SE_sessionid !== md5("$session->SE_username$session->SE_acl$session->SE_time") || !SessionDAO::checkSession($session)) {
+        Core::redirect("index.php");
+    }
+} catch (Exception $e) {
+    Core::redirect("index.php");
 }
 
 // update session timestamp
-SessionDAO::updateSessionTimeout($session->SE_sessionid);
+try {
+    SessionDAO::updateSessionTimeout($session->SE_sessionid);
+} catch (Exception $e) {
+    $database->log(sprintf(_("Internal error, cannot update session timeout: %s"), $session->SE_sessionid), Log::LEVEL_CRITICAL);
+    $core::redirect('index.php', _("Internal error"));
+}
 
 $my = $_SESSION['USER'];
 
-if ($option == 'com_bankaccount') {
-	if ($task == 'download') {
-		$lid = Utils::getParam($_REQUEST, 'EL_emaillistid', null);
+if ($option === 'com_bankaccount' && $task === 'download') {
+    $lid = $_REQUEST['EL_emaillistid'];
 
-		try {
-			$emailList = EmailListDAO::getEmailListByID($lid);
-		} catch (Exception $e) {
-			exit();
-		}
-		header("Content-Description: File Transfer");
-		header("Content-Type: application/force-download");
-		header("Content-Disposition: attachment; filename=\"$emailList->EL_name\"");
-		echo $emailList->EL_list;
-	}
+    try {
+        $emailList = EmailListDAO::getEmailListByID($lid);
+    } catch (Exception $e) {
+        exit();
+    }
+    header("Content-Description: File Transfer");
+    header("Content-Type: application/force-download");
+    header("Content-Disposition: attachment; filename=\"$emailList->EL_name\"");
+    echo $emailList->EL_list;
 }
-?>
--- site/logout.php
+++ site/logout.php
@@ -13,26 +13,23 @@
  */
 
 global $core;
-require_once($core->getAppRoot() . "includes/dao/GroupDAO.php");
-require_once($core->getAppRoot() . "includes/dao/ChargeDAO.php");
-require_once($core->getAppRoot() . "includes/dao/InternetDAO.php");
-require_once($core->getAppRoot() . "includes/dao/IpDAO.php");
-require_once($core->getAppRoot() . "includes/dao/NetworkDAO.php");
-require_once($core->getAppRoot() . "includes/dao/PersonDAO.php");
-require_once($core->getAppRoot() . "includes/dao/RoleDAO.php");
+require_once $core->getAppRoot() . "includes/dao/GroupDAO.php";
+require_once $core->getAppRoot() . "includes/dao/ChargeDAO.php";
+require_once $core->getAppRoot() . "includes/dao/InternetDAO.php";
+require_once $core->getAppRoot() . "includes/dao/IpDAO.php";
+require_once $core->getAppRoot() . "includes/dao/NetworkDAO.php";
+require_once $core->getAppRoot() . "includes/dao/PersonDAO.php";
+require_once $core->getAppRoot() . "includes/dao/RoleDAO.php";
 
 // remove this session from session table
-//
-if (isset($_SESSION['SE_sessionid']) && $_SESSION['SE_sessionid']!="") {
-    (SessionDAO::removeSessionByID($_SESSION['SE_sessionid']));
+if (isset($_SESSION['SE_sessionid']) && $_SESSION['SE_sessionid'] !== "") {
+    SessionDAO::removeSessionByID($_SESSION['SE_sessionid']);
 }
 
-unset($_SESSION["SE_sessionid"]);
-unset($_SESSION["SE_personid"]);
-unset($_SESSION["SE_username"]);
-unset($_SESSION["SE_acl"]);
-unset($_SESSION["SE_logintime"]);
-unset($_SESSION["SE_ip"]);
+unset(
+    $_SESSION["SE_sessionid"], $_SESSION["SE_personid"], $_SESSION["SE_username"],
+    $_SESSION["SE_acl"], $_SESSION["SE_logintime"], $_SESSION["SE_ip"]
+);
 
 if (isset($_SESSION["SE_sessionid"])) {
     session_destroy();
@@ -52,5 +49,4 @@ if (isset($_SESSION["SE_logintime"])) {
 if (isset($_SESSION["SE_ip"])) {
     session_destroy();
 }
-echo "<script>document.location.href='index.php';</script>\n";	
-?>
+echo "<script>document.location.href='index.php';</script>\n";
